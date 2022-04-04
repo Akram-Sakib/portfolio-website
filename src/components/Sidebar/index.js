83b2c19348fc64e10bdd7936e36a5ccfa9e5b1fa
Akram-Sakib <sayedakramsakib@gmail.com>
2022-04-04 12:31:09 +0600

    added some navlink and social link

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,17 +1,84 @@
-import "./index.scss";
-import { Link } from 'react-router-dom';
-import LogoS from "../../assets/images/logo-s.png";
-import LogoSubtitle from "../../assets/images/logo_sub.png";
+import './index.scss'
+import { Link, NavLink } from 'react-router-dom'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faFacebook,
+  faGithub,
+  faLinkedin,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar = () => {
-    return (
-        <div className='nav-bar'>
-            <Link className='logo'>
-                <img src={LogoS} alt="logo" />
-                <img className="sub-logo" src={LogoSubtitle} alt="slogan"/>
-            </Link>
-        </div>
-    );
-};
+  return (
+    <div className="nav-bar">
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="slogan" />
+      </Link>
+      <nav>
+        <NavLink to="/" exact="true" activeClassName="active">
+          <FontAwesomeIcon color="#4d4d4e" icon={faHome} />
+        </NavLink>
+        <NavLink
+          to="/about"
+          exact="true"
+          activeClassName="active"
+          className="about-link"
+        >
+          <FontAwesomeIcon color="#4d4d4e" icon={faUser} />
+        </NavLink>
+        <NavLink
+          to="/contact"
+          exact="true"
+          activeClassName="active"
+          className="contact-link"
+        >
+          <FontAwesomeIcon color="#4d4d4e" icon={faEnvelope} />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/akram-sakib-a7742a214/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/Akram-Sakib"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.facebook.com/akramsakibA"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faFacebook} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://twitter.com/AkramSakib4"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  )
+}
 
-export default Sidebar;
+export default Sidebar
