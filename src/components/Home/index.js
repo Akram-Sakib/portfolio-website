c0feae309e7ffeb84479766f70e02cd2e4e7ddcb
Akram-Sakib <sayedakramsakib@gmail.com>
2022-04-06 10:32:16 +0600

    Final Optimization

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,15 +1,15 @@
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import LogoTitle from "../../assets/images/logo-s.png";
-import AnimatedLetters from "../AnimatedLatters";
-import Logo from "./Logo";
-import "./index.scss";
-import Loader from "react-loaders";
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import LogoTitle from '../../assets/images/logo-s.png'
+import Logo from './Logo'
+import './index.scss'
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
 
-  const [letterClass, setLetterClass] = useState("text-animate");
-  const nameArray = [ 'a', 'k', 'i', 'b'];
+  const nameArray = ['a', 'k', 'i', 'b'];
   const jobArray = [
     'w',
     'e',
@@ -24,48 +24,52 @@ const Home = () => {
     'p',
     'e',
     'r',
-    '.'
+    '.',
   ]
 
-  useEffect(()=>{
-    return setTimeout(()=>{
-      setLetterClass("text-animate-hover")
-    },4000)
-  },[])
+  useEffect(() => {
+    return setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
 
-    return (
-      <>
-        <div className="container home-page">
-          <div className="text-zone">
-            <h1>
-              <span className={letterClass}>H</span>
-              <span className={`${letterClass} _12`}>i,</span>
-              <br />
-              <span className={`${letterClass} _13`}>I</span>
-              <span className={`${letterClass} _14`}>'m</span>
-              <img src={LogoTitle} alt="developer" />
-              <AnimatedLetters
-                letterClass={letterClass}
-                strArray={nameArray}
-                idx={15}
-              />
-              <br />
-              <AnimatedLetters
-                letterClass={letterClass}
-                strArray={jobArray}
-                idx={22}
-              />
-            </h1>
-            <h2>Frontend Developer / Javascript Expert / Student</h2>
-            <Link to="/contact" className="flat-button">
-              CONTACT ME
-            </Link>
-          </div>
-          <Logo />
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img
+              src={LogoTitle}
+              alt="JavaScript Developer Name, Web Developer Name"
+            />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={22}
+            />
+          </h1>
+          <h2>Full Stack Developer / JavaScript Expert / Student</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
         </div>
-        <Loader type="pacman"/>
-      </>
-    )
-};
+        <Logo />
+      </div>
 
-export default Home;
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Home
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,42 +1,34 @@
 import './index.scss'
-import { Link, NavLink } from 'react-router-dom'
 import LogoS from '../../assets/images/logo-s.png'
-import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
-  faFacebook,
-  faGithub,
   faLinkedin,
+  faGithub,
   faTwitter,
+  faFacebook,
 } from '@fortawesome/free-brands-svg-icons'
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { Link, NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
   return (
     <div className="nav-bar">
       <Link className="logo" to="/">
-        <img src={LogoS} alt="logo" />
-        {/* <img className="sub-logo" src={LogoSubtitle} alt="slogan" /> */}
+        <img src={LogoS} alt="Logo" />
       </Link>
       <nav>
-        <NavLink to="/" exact="true" activeClassName="active">
-          <FontAwesomeIcon color="#4d4d4e" icon={faHome} />
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink activeclassname="active" className="about-link" to="/about">
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
         </NavLink>
         <NavLink
-          to="/about"
-          exact="true"
-          activeClassName="active"
-          className="about-link"
-        >
-          <FontAwesomeIcon color="#4d4d4e" icon={faUser} />
-        </NavLink>
-        <NavLink
-          to="/contact"
-          exact="true"
-          activeClassName="active"
+          activeclassname="active"
           className="contact-link"
+          to="/contact"
         >
-          <FontAwesomeIcon color="#4d4d4e" icon={faEnvelope} />
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
         </NavLink>
       </nav>
       <ul>
@@ -44,36 +36,36 @@ const Sidebar = () => {
           <a
             href="https://www.linkedin.com/in/akram-sakib-a7742a214/"
             target="_blank"
-            rel="noopener noreferrer"
+            rel="noreferrer"
           >
             <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
           </a>
         </li>
         <li>
           <a
-            href="https://github.com/Akram-Sakib"
+            href="https://github.com/Akram-Sakib/"
             target="_blank"
-            rel="noopener noreferrer"
+            rel="noreferrer"
           >
             <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
           </a>
         </li>
         <li>
           <a
-            href="https://www.facebook.com/akramsakibA"
+            href="https://twitter.com/AkramSakib4"
+            rel="noreferrer"
             target="_blank"
-            rel="noopener noreferrer"
           >
-            <FontAwesomeIcon icon={faFacebook} color="#4d4d4e" />
+            <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
           </a>
         </li>
         <li>
           <a
-            href="https://twitter.com/AkramSakib4"
+            href="https://www.facebook.com/akramSakibA"
+            rel="noreferrer"
             target="_blank"
-            rel="noopener noreferrer"
           >
-            <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
+            <FontAwesomeIcon icon={faFacebook} color="#4d4d4e" />
           </a>
         </li>
       </ul>
